e35ba6c9dde800b3fa2c298dbdfcf09baa6a4771
Petar Cholakov <petar.cholakov30@gmail.com>
2022-12-13 15:23:50 +0000

    update on navigation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector('.toggle')
 const nav = document.querySelector('.navbar')
 
 		toggle.addEventListener('click', () => {
+			document.body.classList.toggle('no-scroll')
 			toggle.classList.toggle('active')
 			nav.classList.toggle('active')
 
